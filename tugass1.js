6a2d81b69b9b2ceb63b01d917d74be466d267b46
dela9 <124057172+dela9@users.noreply.github.com>
2023-09-29 21:12:03 +0700

    Add files via upload

---
+++ tugass1.js
@@ -0,0 +1,15 @@
+let identitas = 'identitas'
+console.log('identitas')
+var nama = 'FEBI SALSABILA AZALI '
+let nim ='20220040278'
+console.log('nama saya = ' + nama )
+console.log('nim saya = ' + nim )
+// aritmatika
+let aritmatika = 'aritmatika'
+console.log(aritmatika)
+let a = 29;
+let b = 9;
+console.log(a + ' * ' + b + ' = ' +a*b)
+console.log(a + ' + ' + b + ' = ' +a+b)
+console.log(a + ' / ' + b + ' = ' +a/b)
+console.log(a + ' - ' + b + ' = ' +a-b)
